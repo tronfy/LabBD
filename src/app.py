77a27e0f13407a811efbd36e09f8f7f04ce155df
Nic Schmidt <nicolasdschmidt@gmail.com>
2024-09-27 18:35:41 +0000

    mysql connection

--- src/app.py
+++ src/app.py
@@ -2,18 +2,22 @@ import mysql.connector
 import streamlit as st
 import pandas as pd
 
+USE_DOCKER = True
+
 con = mysql.connector.connect(
-  host="localhost",
-  port="33061",
-  user="root",
-  password="c6h5bwInqtKJKmiuPcBQ0A",
-  database="censo"
+    host="localhost",
+    port="33061" if USE_DOCKER else "3306",
+    user="root",
+    password="c6h5bwInqtKJKmiuPcBQ0A" if USE_DOCKER else "aluno",
+    database="censo" if USE_DOCKER else "labbd",
 )
 
 cur = con.cursor()
 
-cur.execute("select * from Escola limit 20")
+cur.execute("select CO_ENTIDADE, NO_ENTIDADE from Escola limit 20")
 rows = cur.fetchall()
+
+rows = pd.DataFrame(rows, columns=["CO_ENTIDADE", "NO_ENTIDADE"])
 
 st.header("Escolas")
 st.write(rows)
