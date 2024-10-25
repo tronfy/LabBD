2868cfee95ec2889ff3483bf22df434b120174af
Nic Schmidt <nicolasdschmidt@gmail.com>
2024-10-25 18:20:22 +0000

    use lf endings

--- queries/2_import_base_data.sql
+++ queries/2_import_base_data.sql
@@ -1,13 +1,13 @@
 set sql_mode = "";
 
 load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\Escola.csv' into
-table labbd.escola fields terminated by '|' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
+table labbd.escola fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
 
 load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\Turma.csv' into
-table labbd.turma fields terminated by '|' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
+table labbd.turma fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
 
 load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\Docente.csv' into
-table labbd.docente fields terminated by '|' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
+table labbd.docente fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
 
 load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\Matricula.csv' into
-table labbd.matricula fields terminated by '|' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
+table labbd.matricula fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
