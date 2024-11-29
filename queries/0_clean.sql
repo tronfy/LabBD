cfd71ac62a4b5766287d601df141f1dea75d4afa
Nic Schmidt <nicolasdschmidt@gmail.com>
2024-11-29 17:53:31 +0000

    uncomment sql files

--- queries/0_clean.sql
+++ queries/0_clean.sql
@@ -1,13 +1,13 @@
--- use labbd;
+use labbd;
 
--- drop table if exists bookmark;
+drop table if exists bookmark;
 
--- drop table if exists usuario;
+drop table if exists usuario;
 
--- drop table if exists matricula;
+drop table if exists matricula;
 
--- drop table if exists docente;
+drop table if exists docente;
 
--- drop table if exists turma;
+drop table if exists turma;
 
--- drop table if exists escola;
+drop table if exists escola;
--- queries/2_import_base_data.sql
+++ queries/2_import_base_data.sql
@@ -1,13 +1,13 @@
--- set sql_mode = "";
+set sql_mode = "";
 
--- load data infile '/var/lib/mysql-files/Escola.csv' into
--- table labbd.escola fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
+load data infile '/var/lib/mysql-files/Escola.csv' into
+table labbd.escola fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
 
--- load data infile '/var/lib/mysql-files/Turma.csv' into
--- table labbd.turma fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
+load data infile '/var/lib/mysql-files/Turma.csv' into
+table labbd.turma fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
 
--- load data infile '/var/lib/mysql-files/Docente.csv' into
--- table labbd.docente fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
+load data infile '/var/lib/mysql-files/Docente.csv' into
+table labbd.docente fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
 
--- load data infile '/var/lib/mysql-files/Matricula.csv' into
--- table labbd.matricula fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
+load data infile '/var/lib/mysql-files/Matricula.csv' into
+table labbd.matricula fields terminated by '|' enclosed by '"' lines terminated by '\n' ignore 1 lines;
